49d3cf94304d8cae42a4fa5b5d317deb2499bc01
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-18 02:37:45 -0600

    Finsihed testing addToCollection function, added 6 albums to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,32 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+// created new variable collection and set it to an empty array
+function addToCollection (albumTitle,artist,yearPublished,genre) {
+let album = {
+    albumTitle: albumTitle,
+    artist: artist,
+    yearPublished: yearPublished,
+    genre: genre,
+} // created a function addToCollection that takes 4 arguments
+//added object album to the function 
+collection.unshift(album) // added the object (album) and added it to collection array
+return album
+};
+addToCollection('For The Cool in You', 'Babyface', '1993', 'R&B');
+console.log("Added For The Cool in You by Babyface", collection);
+
+addToCollection('Never Say Never', 'Brandy', '1998', 'R&B');
+console.log("Added Never Say Never Album by Brandy", collection);
+
+addToCollection('Exclusive', 'Chris Brown', '2007', 'R&B');
+console.log('Added Exclusive Album by Chris Brown', collection);
+
+addToCollection('Take Care', 'Drake', '2011', 'Hip Hop');
+console.log('Added Take Care Album by Drake', collection);
+
+addToCollection('The Documentary', 'The Game', '2005','Rap');
+console.log('Added The Documentary Album by The Game', collection);
+
+addToCollection('Late Registration', 'Kanye West', '2005', 'Rap');
+console.log('Added Late Registration by Kanye West', collection);
